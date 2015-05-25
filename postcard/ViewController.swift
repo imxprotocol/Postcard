6b23b26f6c71267608b7637e6d415e8071346059
AI Sense MacMini <aisense_macmini@AIs-Mac-mini.local>
2015-05-26 03:00:26 +0800

    Initial Commit

---
+++ postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  postcard
+//
+//  Created by AI Sense MacMini on 5/26/15.
+//  Copyright (c) 2015 AI Sense. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
